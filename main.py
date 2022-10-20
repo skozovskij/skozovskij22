31dd3e7cfcbccd31a663771324fee85006911aa5
skozovskij22 <skozovskij@gmail.com>
2022-10-20 18:31:43 +0300

    initial commit

--- main.py
+++ main.py
@@ -8,3 +8,18 @@ nick = Student()
 kate = Student(height=170)
 print(nick.height)
 print(kate.height)
+
+
+x = 10
+class Looker:
+    x = 15
+    print(x)
+    def func_1(self):
+        x = 20
+        print(x)
+        def func_2():
+            x = 30
+            print(x)
+        func_2()
+some_object = Looker()
+some_object.func_1()
